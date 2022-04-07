3480c00e7af563e2a5f38ec99114fc37efdd509e
thinkabyaroo <thinkabyaroo2911@gmail.com>
2022-04-07 13:36:04 +0630

    first

---
+++ src/components/Product.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="col-6 col-md-4 col-xl-3">
+      <div class="card">
+        <div class="card-body">
+          <figure>
+            <img :src="productDetail.image" class="product-img" alt="">
+          </figure>
+          <p>{{productDetail.title}}</p>
+
+          <div class="d-flex justify-content-between">
+            <p>${{productDetail.price}}</p>
+              <Rating :rating="productDetail.rating"></Rating>
+          </div>
+          <button class="btn btn-outline-dark w-100">Add to cart</button>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import Rating from "@/components/Rating";
+export default {
+  components: {Rating},
+  props: ["productDetail"],
+}
+</script>
+
+<style >
+.product-img{
+  height: 100px;
+  width: 100px;
+}
+
+</style>
